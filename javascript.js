67b05ef7e4931d6e971c510a11843232c665faa1
Maram <37120344+MaramAlqadasi@users.noreply.github.com>
2023-08-31 07:14:38 +0000

    add  showResult function

--- javascript.js
+++ javascript.js
@@ -4,38 +4,37 @@ function computerChoice(){
     return choice[randomChoice];
 }
 //console.log(computerChoice());
+function showResult(massage,computerSelection,playerSelection,computerScore,playerScore){
+    console.log (`${massage} Computer :${computerSelection}  You: ${playerSelection}\n`);
+    console.log(`Computer Score : ${computerScore}\nYour Score : ${playerScore}`);
+
+}
 function playRound(computerSelection,playerSelection){
     let playerScore=0;
     let computerScore=0;
     if(computerSelection===playerSelection){
-        console.log ("it is a tie" + " "+`Computer :${computerSelection}+ " "+ You: ${playerSelection}`);
-        console.log(`Computer Score : ${computerScore} + "  " + Your Score : ${playerScore}`);
+        showResult("it is a tie",computerSelection,playerSelection,computerScore,playerScore);
     }
     else if(computerSelection==="Rock"){
         if(playerSelection==="Paper"){
             playerScore+=1;
-            console.log ("You have a point " + " "+`Computer :${computerSelection}+ " "+ You: ${playerSelection}`);
-            console.log(`Computer Score : ${computerScore} + "  " + Your Score : ${playerScore}`);
+            showResult("You have a point " ,computerSelection,playerSelection,computerScore,playerScore);
 
         }
         else{
             computerScore+=1;
-            console.log ("You have a point " + " "+`Computer :${computerSelection}+ " "+ You: ${playerSelection}`);
-            console.log(`Computer Score : ${computerScore} + "  " + Your Score : ${playerScore}`);
+            showResult("Computer have a point " ,computerSelection,playerSelection,computerScore,playerScore);
 
         }
     }
     else if (computerSelection==="Paper"){
         if(playerSelection==="Scissors"){
             playerScore+=1;
-            console.log ("You have a point " + " "+`Computer :${computerSelection}+ " "+ You: ${playerSelection}`);
-            console.log(`Computer Score : ${computerScore} + "  " + Your Score : ${playerScore}`);
-
+            showResult("You have a point " ,computerSelection,playerSelection,computerScore,playerScore);
         }
         else{
             computerScore+=1;
-            console.log ("You have a point " + " "+`Computer :${computerSelection}+ " "+ You: ${playerSelection}`);
-            console.log(`Computer Score : ${computerScore} + "  " + Your Score : ${playerScore}`);
+            showResult("Computer have a point " ,computerSelection,playerSelection,computerScore,playerScore);
 
         }
 
